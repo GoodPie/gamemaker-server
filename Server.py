dfb8903d6f555e483fb736c167b6826398f5b5b4
Brandyn Britton <brandynbb96@gmail.com>
2017-06-25 10:49:56 +1000

    Added a bit of commenting

--- Server.py
+++ Server.py
@@ -6,21 +6,22 @@ from User import User
 
 
 class Server:
+    def __init__(self, max_clients, port):
 
-    def __init__(self, clients, port):
-
-        self.clients = clients
+        self.max_clients = max_clients
+        self.users = []
         self.port = port
         self.socket = None
         self.running = False
 
     def start(self):
 
+        # Create a new socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
-        print("Successfully created socket...")
+        # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
 
         try:
+            # Attempt to bind socket to port
             self.socket.bind(("", self.port))
             self.running = True
         except socket.error as err:
@@ -29,12 +30,16 @@ class Server:
 
         # Main server loop
         while self.running:
-
             sleep(1 / 1000)
 
-            self.socket.listen(self.clients)
-            connection, address = self.socket.accept()
-            print("Connected to {0}:{1}".format(address[0], address[1]))
-            user = User(connection, self)
-            user.start()
+            # Listen for incoming connections
+            self.socket.listen(self.max_clients)
 
+            # Accept incoming connections
+            connection, address = self.socket.accept()
+
+            # Add user to user list and start the user thread
+            print("Connected to {0}:{1}".format(address[0], address[1]))
+            user = User(connection, address, self)
+            user.start()
+            self.users.append(user)
--- User.py
+++ User.py
@@ -4,22 +4,35 @@ import socket
 
 
 class User(threading.Thread):
-    def __init__(self, connection, server):
+    def __init__(self, connection, address, server):
         # Initialize the thread
         threading.Thread.__init__(self)
 
         # Initialize class fields
         self.connection = connection
+        self.address = address
         self.server = server
         self.connected = True
 
     def run(self):
         while self.connected:
-            # Receive data from clients
-            data = self.connection.recv(1024)
-            event_id = struct.unpack('B', data[:1])[0]
 
-            # Determine what to do based on event id
-            if event_id == 0:
-                print(self.connection.send(data))
+            try:
+                # Receive data from clients
+                data = self.connection.recv(1024)
 
+                # Determine what to do based on event id
+                event_id = struct.unpack('B', data[:1])[0]
+                if event_id == 0:
+                    self.connection.send(data)
+            except ConnectionResetError:
+                self.disconnect_user()
+            except struct.error:
+                # This only appears when the user attempts to disconnect during loop
+                # Will probably be removed when more data is handled so we can properly handle this error
+                self.disconnect_user()
+
+    def disconnect_user(self):
+        print("Disconnected from {0}:{1}".format(self.address[0], self.address[1]))
+        self.server.users.remove(self)
+        self.connected = False
